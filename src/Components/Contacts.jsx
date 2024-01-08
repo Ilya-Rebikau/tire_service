14a95bc741f498e484239c4995afb1f642ebb1df
Ilya-Rebikau <ilya.rebikov.2000@mail.ru>
2024-01-08 13:30:35 +0300

    Добавлены отзывы

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -33,7 +33,7 @@ export default class Contacts extends Component {
         return (
             <div className="container px-3 py-2">
                 <div className="d-flex row border rounded border-dark bg-light bg-gradient px-3 py-3">
-                    <section className="col-auto me-5">
+                    <section className="col-auto">
                         <h2 className="text-center"><img src={map} alt="map" /> Мы на карте</h2>
                         <YMaps>
                             <Map
@@ -57,19 +57,36 @@ export default class Contacts extends Component {
                     </section>
                     <section className="col-auto flex-grow-1">
                         <article>
-                            <h2 className="fs-1 text-center"><img src={tire} alt="tire" /> Наши контакты <img src={tire} alt="tire" /></h2>
+                            <h2 className="fs-1 text-center">
+                                <img src={tire} alt="tire" /> 
+                                Наши контакты 
+                                <img src={tire} alt="tire" />
+                            </h2>
                             <p className="fs-3">Геннадий: <a href="tel:+375 (29) 364-19-78" ><img src={phone} alt="phone" />+375(29)364-19-78</a>, <a href="mailto:rebikov71@bk.ru"><img src={email} alt="email" /> rebikov71@bk.ru</a></p>
                             <p className="fs-3">Александр: <a href="tel:+375 (29) 658-32-01" ><img src={phone} alt="phone" />+375(29)658-32-01</a></p>
-                            <div className="row d-flex justify-content-center">
-                                <img className="col-auto" src={tire} alt="tire" />
-                                <img className="col-auto" src={tire} alt="tire" />
-                                <img className="col-auto" src={tire} alt="tire" />
-                                <img className="col-auto" src={tire} alt="tire" />
-                            </div>
                         </article>
                         <article>
-                            <h2 className="text-center fs-1">Время работы</h2>
+                            <h2 className="text-center fs-1">
+                                <img className="col-auto" src={tire} alt="tire" /> 
+                                Время работы 
+                                <img className="col-auto" src={tire} alt="tire" />
+                            </h2>
                             <p className="fs-3 text-center"><img src={time} alt="time" /> 7 дней в неделю с 8:00 до 20:00</p>
+                        </article>
+                        <article>
+                            <h2 className="fs-1 text-center">
+                                <img className="col-auto" src={tire} alt="tire" /> 
+                                Нам важны Ваши отзывы 
+                                <img className="col-auto" src={tire} alt="tire" />
+                            </h2>
+                            <div className="row d-flex justify-content-center">
+                                <a className="fs-3 text-center" href="https://yandex.by/profile/91289259818">
+                                    Оценки и отзывы - Яндекс
+                                </a>
+                                <a className="fs-3 text-center" href="https://g.page/r/CcncRpsec0XTEAI/review">
+                                    Оценки и отзывы - Google
+                                </a>
+                            </div>
                         </article>
                     </section>
                 </div>
